415dad9830cf16a3081af6f728dee8cc770a0d7f
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-31 23:08:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,16 @@
 # Write your code here!
+def game_hash 
+game_hash={
+  :home => {
+      :team_name => "team_name", 
+      :colors =>"color1, color2", 
+      :players => []
+  },
+  :away=> {
+      :team_name => "team_name", 
+      :colors =>"color1, color2", 
+      :players => []
+  }
+}
+return game_hash 
+end 
